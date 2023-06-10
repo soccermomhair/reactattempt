9cff305c3d9461420cbca770f7927b763e358c9c
lxschutz <lxschutz@gmail.com>
2023-06-10 17:10:29 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, Dojo!</h1>
+      </header>
+      <p>Things I need to do:</p>
+      <ul>
+        <li>Learn React</li>
+        <li>Affect real political change</li>
+        <li>Realize an emotionally vulnerable work of art</li>
+        <li>Learn to rollerblade</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
